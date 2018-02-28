14b715925a1aae944181a257fdfec1e659b8904e
Ben Robertson <benjamingrobertson@me.com>
2018-02-28 16:59:23 -0500

    Create inital hello world schema.

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,4 +1,27 @@
 import { createApolloServer } from 'meteor/apollo';
 import { makeExecutableSchema } from 'graphql-tools';
 
-createApolloServer({});
+// Schema
+const typeDefs = `
+  type Query {
+    hi: String
+  }
+`;
+
+// Server Side code
+const resolvers = {
+  Query: {
+    hi() {
+      return 'Hello World';
+    }
+  }
+}
+
+const schema = makeExecutableSchema({
+  typeDefs,
+  resolvers
+})
+
+createApolloServer({
+  schema
+});
