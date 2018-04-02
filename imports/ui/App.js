690a98996773afd632d890cfd8371b199ba4fb2d
Ben Robertson <benjamingrobertson@me.com>
2018-04-02 19:58:37 -0400

    Enable user login, update page on login.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -1,18 +1,37 @@
 import React from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
+import { withApollo } from 'react-apollo';
 
 import ChartEntryForm from './ChartEntryForm';
+import RegisterForm from './login/RegisterForm';
+import LoginForm from './login/LoginForm';
 
-const App = ({ loading, chartEntries }) => {
+const App = ({ loading, chartEntries, client }) => {
   if (loading) return null;
   return (
     <div>
       <h1>Charting</h1>
+      <p>Welcome userName</p>
+      <h2>Register</h2>
+      <RegisterForm client={client} />
+      <h2>Login</h2>
+      <LoginForm client={client} />
+      <button onClick={() => {
+        Meteor.logout();
+        client.resetStore();
+      }}>Logout</button>
+      <h2>New Entry</h2>
       <ChartEntryForm />
       <ul>
         {chartEntries.map(chartEntry => (
-          <li key={chartEntry._id}>{chartEntry.date}</li>
+          <li key={chartEntry._id} >
+            <span>date: {chartEntry.date}</span><br />
+            <span>consistency: {chartEntry.consistency}</span><br />
+            <span>frequency: {chartEntry.frequency}</span><br />
+            <span>flow: {chartEntry.flow}</span><br />
+            <span>intercourse: {chartEntry.intercourse}</span><br />
+          </li>
         ))}
       </ul>
     </div>
@@ -21,13 +40,17 @@ const App = ({ loading, chartEntries }) => {
 
 const chartEntriesQuery = gql`
   query chartEntries {
-    chartEntries {
+        chartEntries {
       _id,
-      date
+      date,
+      consistency,
+      frequency,
+      flow,
+      intercourse
     }
   }
 `;
 
 export default graphql(chartEntriesQuery, {
   props: ({ data }) => ({ ...data })
-})(App);
+})(withApollo(App));
