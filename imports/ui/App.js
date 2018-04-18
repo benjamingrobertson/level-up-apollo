39ccc9ff3c48c5e360dda968971f172ae212257d
Ben Robertson <benjamingrobertson@me.com>
2018-04-18 13:36:14 -0400

    Clean up the login / registration UI.

--- imports/ui/App.js
+++ imports/ui/App.js
@@ -6,49 +6,37 @@ import { withApollo } from 'react-apollo';
 import GoalForm from './GoalForm';
 import Goal from './resolutions/Goal';
 import ResolutionForm from './ResolutionForm';
-import RegisterForm from './RegisterForm';
-import LoginForm from './LoginForm';
+
+import UserForm from './UserForm';
 
 const App = ({ loading, resolutions, client, user }) => {
   if (loading) return null;
   return (
     <div>
-      {user._id ? (
-        <button
-          onClick={() => {
-            Meteor.logout();
-            client.resetStore();
-          }}
-        >
-          Logout
-        </button>
-      ) : (
-        <div>
-          <RegisterForm client={client} />
-          <LoginForm client={client} />
-        </div>
-      )}
+      <UserForm user={user} client={client} />
 
-      <ResolutionForm />
-      <ul>
-        {resolutions.map((resolution) => (
-          <li key={resolution._id}>
-            <span
-              style={{
-                textDecoration: resolution.completed ? 'line-through' : 'none'
-              }}
-            >
-              {resolution.name}
-            </span>
-            <ul>
-              {resolution.goals.map((goal) => (
-                <Goal goal={goal} key={goal._id} />
-              ))}
-            </ul>
-            <GoalForm resolutionId={resolution._id} />
-          </li>
-        ))}
-      </ul>
+      {user._id && <ResolutionForm />}
+      {user._id && (
+        <ul>
+          {resolutions.map((resolution) => (
+            <li key={resolution._id}>
+              <span
+                style={{
+                  textDecoration: resolution.completed ? 'line-through' : 'none'
+                }}
+              >
+                {resolution.name}
+              </span>
+              <ul>
+                {resolution.goals.map((goal) => (
+                  <Goal goal={goal} key={goal._id} />
+                ))}
+              </ul>
+              <GoalForm resolutionId={resolution._id} />
+            </li>
+          ))}
+        </ul>
+      )}
     </div>
   );
 };
